53f90ddddd0f6c24edf18b8b883c8c40d3fd590c
freezykidakingv2 <freezyfreezer201@gmail.com>
2025-09-13 11:34:51 +0100

    Update goodW.cpp

--- Find the Word/goodW.cpp
+++ Find the Word/goodW.cpp
@@ -2,7 +2,7 @@
 
 int check(std::vector<char> wordC, std::string bLC) {
 	
-	for (int l : bLC) {
+	for (char l : bLC) {
 		if (std::find(wordC.begin(), wordC.end(), l) != wordC.end()) {
 			return 1;
 		}
